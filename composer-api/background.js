3f34546562855d1759e8a929e9f979be9c9bd8c1
Achim <git@achimonline.de>
2021-01-12 17:57:03 +0100

    bug resolved

--- composer-api/background.js
+++
@@ -1,9 +0,0 @@
-browser.composeAction.onClicked.addListener(async (tab) => {
-  let details = await browser.compose.getComposeDetails(tab.id);
-
-  if (details.isPlainText) {
-    let body = details.plainTextBody;
-    console.log(body);
-    browser.compose.setComposeDetails(tab.id, { plainTextBody: body });
-  }
-});
